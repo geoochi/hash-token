a55a6ef724b9d051e2632658d86eada6f9cf441f
Geoo Chi <geoooochi@gmail.com>
2025-11-19 23:08:05 +0800

    update

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, session, request, redirect, url_for
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+if 'FLASK_SECRET_KEY' not in os.environ:
+    raise Exception('Error: no FLASK_SECRET_KEY')
+
+kv = json.load(open('kv.json'))
+
+app = Flask(__name__, template_folder='templates')
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
+
+@app.route('/')
+def index():
+    params = request.args
+    if session.get('verified', False):
+        return render_template('index.html')
+    else:
+        hash = params.get('hash', None)
+        if hash is None:
+            return render_template('error.html', msg='403 error')
+        elif hash in kv:
+            if not kv[hash]:
+                kv[hash] = True
+                json.dump(kv, open('kv.json', 'w'), indent=2)
+                session['verified'] = True
+                return redirect(url_for('index'))
+            else:
+                return render_template('error.html', msg='403 error: hash used')
+        else:
+            return render_template('error.html', msg='403 error: hash invalid')
+
+
+if __name__ == '__main__':
+    app.run(port=5002)
